c1cb6a589ff0a85c02429016d822792ef148bb63
Jean-Fabian Wenisch <jean-fabian@wenisch.tech>
2026-09-03 22:47:39 +0200

    fix: fixed problem leading to nav unintended appearing at the bottom of pages

--- src/test/java/tech/wenisch/contextcrate/web/FrontendMigrationTest.java
+++ src/test/java/tech/wenisch/contextcrate/web/FrontendMigrationTest.java
@@ -19,7 +19,9 @@ class FrontendMigrationTest {
   @Test
   void largeConfigurationFormsExposeAtomicWizardControls() throws IOException {
     assertThat(Files.readString(Path.of("src/main/resources/templates/ingestion-job-form.html")))
-        .contains("x-data=\"wizard(5)\"", "Ready to save", "Save ingestion job");
+        .contains("x-data=\"wizard(5)\"", "Ready to save", "Save ingestion job",
+            "fragments :: sidebar", "fragments :: overlay", "fragments :: topbar(${job == null ? 'Create ingestion job' : 'Edit ingestion job'})")
+        .doesNotContain("fragments :: nav");
     assertThat(Files.readString(Path.of("src/main/resources/templates/settings.html")))
         .contains("x-data=\"wizard(4)\"", "Retrieval &amp; answers", "Review &amp; save");
   }
